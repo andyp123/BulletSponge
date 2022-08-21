682f95843ad8dd6ecd0fcd4babfb3efe07215a0d
Andrew Palmer <andyp.123@gmail.com>
2022-08-22 00:06:36 +0900

    Add space partition system (currently testing). Not sure how useful this is!

--- source/BulletManager.lua
+++ source/BulletManager.lua
@@ -39,6 +39,17 @@ function BulletManager.new(maxBullets)
 end
 
 
+function BulletManager:enableSpacePartition()
+	-- 400/40, 240/40, 16 bullets per bucket max
+	local spacePartition = SpacePartition.new(10, 6, 16)
+
+	local x, y, w, h = BulletManager.bounds:unpack()
+	spacePartition:updateBounds(x, y, w, h)
+	
+	self.spacePartition = spacePartition
+end
+
+
 function BulletManager:update()
 	local bullets = self.bullets
 	local deltaTime = deltaTimeSeconds
@@ -148,3 +159,106 @@ function Bullet:init(x, y, vx, vy, lifetime)
 	self.vy = vy
 	self.lifetime = lifetime
 end
+
+
+SpacePartition = {}
+SpacePartition.__index = SpacePartition
+
+function SpacePartition.new(bucketsX, bucketsY, maxObjectsPerBucket)
+	local numBuckets = bucketsX * bucketsY
+	local buckets = table.create(numBuckets)
+	for i = 1, numBuckets do
+		buckets[i] = {
+			objects = table.create(maxObjectsPerBucket),
+			count = 0,
+		}
+	end
+
+	local w, h = 400, 240
+	local a = {
+		xMin = 0,
+		yMin = 0,
+		width = w,
+		height = h,
+		bucketWidth = w / bucketsX,
+		bucketHeight = h / bucketsY,
+		bucketsX = bucketsX,
+		bucketsY = bucketsY,
+		buckets = buckets,
+		maxObjectsPerBucket = maxObjectsPerBucket,
+	}
+	setmetatable(a, SpacePartition)
+
+	return a
+end
+
+
+-- Will invalidate entries added already
+function SpacePartition:updateBounds(xMin, yMin, width, height)
+	self.xMin = xMin
+	self.yMin = yMin
+	self.width = width
+	self.height = height
+	self.bucketWidth = width / self.bucketsX
+	self.bucketHeight = height / self.bucketsY
+end
+
+
+
+-- loop through the buckets and clear them all
+function SpacePartition:clear()
+	local numBuckets = bucketsX * bucketsY
+	local buckets = self.buckets
+	for i = 1, numBuckets do
+		local bucket = buckets[i]
+		local objects = bucket.objects
+		for j = 1, self.maxObjectsPerBucket do
+			bucket[j] = nil
+		end
+		bucket.count = 0
+	end	
+end
+
+
+-- Add single object via index
+function SpacePartition:addAtIndex(i, object)
+	local bucket = self.buckets[i]
+	local count = bucket.count
+	if count < self.maxObjectsPerBucket then
+		count += 1
+		bucket[count] = object
+		bucket.count = count
+	end
+end
+
+
+-- Does not check out of bounds
+-- out < 1 <= in <= numBuckets < out
+function SpacePartition:getIndex(x, y)
+	local xi = math.floor((x - self.xMin) / self.bucketWidth)
+	local yi = math.floor((y - self.yMin) / self.bucketHeight)
+	return yi * self.bucketsX + xi + 1
+end
+
+
+function SpacePartition:debugDraw()
+
+end
+
+-- -- NOTE: The bucket tables are garbage so need deleting,
+-- -- but hopefully it's small enough to avoid a hitch
+-- function SpacePartition:clear()
+-- 	local numBuckets = self.bucketsX * self.bucketsY
+-- 	local buckets = self.buckets
+-- 	for i = 1, numBuckets do
+-- 		buckets[i] = {}
+-- 	end
+
+-- 	-- this will occur EVERY FRAME. This might not be good.
+-- 	-- Assuming these tables are the only garbage :)
+-- 	collectgarbage()
+-- end
+
+-- function SpacePartition:addAtIndex(i, object)
+-- 	self.buckets[i]
+-- end
